2d5ab50e11b628edf1c10190b9b79d3c3d9a8b86
Massine <massineabgar@hotmail.com>
2025-02-19 16:26:22 +0100

    Dernieres modifs

--- template/accueil.php
+++ template/accueil.php
@@ -16,16 +16,27 @@
     include 'inc/menu.php';
     ?>
     <main class="body-main">
-    <h1 class="title-main">Bienvenue sur notre site consacré à Berlin</h1>
-    <div class="blason-berlin"><img src="img/armoiries.png" alt="blason" /></div>
-    <p>Berlin est la capitale et la plus grande ville d'Allemagne. Institutionnellement, c’est une ville-État nommée Land de Berlin.
+        <h1 class="title-main">Bienvenue sur notre site consacré à Berlin</h1>
+        <div class="blason-berlin"><img src="img/armoiries.png" alt="blason" /></div>
+        <p>Berlin est la capitale et la plus grande ville d'Allemagne. Institutionnellement, c’est une ville-État nommée Land de Berlin.
 
-        Située dans le Nord-Est du pays, Berlin compte environ 3,8 millions d'habitants. Ses habitants s'appellent les Berlinois et les Berlinoises (die Berliner et die Berlinerinnen en allemand). Elle est la première ville et la huitième agglomération la plus peuplée de l'Union européenne. L'agglomération de Berlin s'étend sur 892 km ², et compte 4,4 millions d'habitants (A titre de comparaison, la métropole du grand Paris s'étend sur 814 km ² et compte 7 millions d'habitants). La région métropolitaine de Berlin-Brandebourg qui cumule les Länder de Berlin et de Brandebourg regroupe au total près de 6 millions d'habitants.</p>
+            Située dans le Nord-Est du pays, Berlin compte environ 3,8 millions d'habitants. Ses habitants s'appellent les Berlinois et les Berlinoises (die Berliner et die Berlinerinnen en allemand). Elle est la première ville et la huitième agglomération la plus peuplée de l'Union européenne. L'agglomération de Berlin s'étend sur 892 km ², et compte 4,4 millions d'habitants (A titre de comparaison, la métropole du grand Paris s'étend sur 814 km ² et compte 7 millions d'habitants). La région métropolitaine de Berlin-Brandebourg qui cumule les Länder de Berlin et de Brandebourg regroupe au total près de 6 millions d'habitants.</p>
 
         <p>Fondée au XIIIe siècle, Berlin a été successivement capitale de l'électorat du Brandebourg (1247-1701), du royaume de Prusse (1701-1871), de l'Empire allemand (1871-1918), de la République de Weimar (1919-1933) et du Troisième Reich (1933-1945). Après 1945 et jusqu'à la chute du mur de Berlin en 1989, la ville est partagée en quatre secteurs d'occupation. Pendant la Guerre froide, le secteur soviétique de la ville, nommé Berlin-Est, est devenu la capitale de la République démocratique allemande, tandis que Berlin-Ouest était politiquement rattachée à la République fédérale d'Allemagne, devenant ainsi un bastion avancé du « Monde libre » à l'intérieur du Bloc communiste. Après la chute du mur, Berlin redevint, en 1990, la capitale de l'Allemagne alors réunifiée, et les principales institutions fédérales y emménagèrent en 1999.</p>
 
-        <p>Berlin est une ville mondiale culturelle et artistique de premier plan. La ville abrite 166 musées, 142 bibliothèques et 60 théâtres. En 2014, Berlin a accueilli 11,87 millions de visiteurs (+4,8 % par rapport à 2013), dont 4,52 millions de visiteurs étrangers (+5,2 %).</p>       
-   </main>
-</body>                    
+        <p>Berlin est une ville mondiale culturelle et artistique de premier plan. La ville abrite 166 musées, 142 bibliothèques et 60 théâtres. En 2014, Berlin a accueilli 11,87 millions de visiteurs (+4,8 % par rapport à 2013), dont 4,52 millions de visiteurs étrangers (+5,2 %).</p>
+    
+        <div class="navAccueil">
+            <ul>
+                <li><a href="?p=geographie">Explorer la géographie de la région</a></li>
+                <li><a href="?p=histoire">Découvrir la riche histoire de Berlin</a></li>
+                <li><a href="?p=culture">Art et Culture des environs</a></li>
+                <li><a href="?p=galerie">Quelques images</a></li>
+                <li><a href="?p=contact">Veuillez nous contacter vers ce lien</a></li>
+                <li><a href="?p=liens">Quelques lien pour en savoir plus...</a></li>
+            </ul>
+        </div>
+    </main>
+</body>
 
 </html>
--- template/inc/menu.php
+++ template/inc/menu.php
@@ -2,6 +2,7 @@
     <ul class="liste">
         <li><a href="?p=./">Accueil</li>
         <li><a href="?p=geographie">Géographie</a></li>
+        <li><a href="?p=histoire">Histoire </a></li>
         <li><a href="?p=culture">Culture</a></li>
         <li><a href="?p=galerie"> Galerie</a></li>
         <li><a href="?p=contact">Contact</a></li>
