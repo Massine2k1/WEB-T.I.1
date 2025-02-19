e98a599e0c2bad5ebd9bccb5055fa1ae2369a8dc
Massine <massineabgar@hotmail.com>
2025-02-19 12:17:05 +0100

    style header/nav-bar

---
+++ template/inc/banniere.php
@@ -0,0 +1,4 @@
+<header class="header">
+    <img src="img/banniere.jpg" alt=Berlin class="img-berlin"/>
+    <h1 class="main-title"><span class="contain-title">Berlin</span></h1>
+</header>
--- template/inc/menu.php
+++ template/inc/menu.php
@@ -1,10 +1,10 @@
-<naV>
-    <ul>
-        <li><a href="?p=accueil.php">Accueil</li>
-        <li><a href="?p=geographie.php">Géographie</a></li>
-        <li><a href="?p=culture.php">Culture</a></li>
-        <li><a href="?p=galerie.php">Galerie</a></li>
-        <li><a href="?p=contact.php">Contact</a></li>
-        <li><a href="?p=liens.php">Liens</a></li>
+<nav class="nav-bar">
+    <ul class="liste">
+        <li><a href="?p=accueil">Accueil</li>
+        <li><a href="?p=geographie">Géographie</a></li>
+        <li><a href="?p=culture">Culture</a></li>
+        <li><a href="?p=galerie"> Galerie</a></li>
+        <li><a href="?p=contact">Contact</a></li>
+        <li><a href="?p=liens">Liens</a></li>
     </ul>
-</naV>
+</nav>
